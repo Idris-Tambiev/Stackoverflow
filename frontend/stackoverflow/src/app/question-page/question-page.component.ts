e9ac17ae6d0188c43509fca7e595a5363bdee0d9
Idris <idris.tambiev.itp@gmail.com>
2021-03-18 17:48:25 +0300

    Created Components

--- frontend/stackoverflow/src/app/question-page/question-page.component.ts
+++ frontend/stackoverflow/src/app/question-page/question-page.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Answer {
+  id: number;
+  text: string;
+  date: string;
+}
 @Component({
   selector: 'app-question-page',
   templateUrl: './question-page.component.html',
   styleUrls: ['./question-page.component.css'],
 })
 export class QuestionPageComponent implements OnInit {
+  answers: Answer[] = [];
   constructor() {}
 
   ngOnInit(): void {}
--- frontend/stackoverflow/src/app/questions-list/questions-list.component.ts
+++ frontend/stackoverflow/src/app/questions-list/questions-list.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Interface } from 'node:readline';
 
-Interface Question{
-
+export interface Question {
+  id: number;
+  text: string;
+  description: string;
+  count: number;
+  date: string;
 }
 
 @Component({
   selector: 'app-questions-list',
   templateUrl: './questions-list.component.html',
-  styleUrls: ['./questions-list.component.css']
+  styleUrls: ['./questions-list.component.css'],
 })
-
 export class QuestionsListComponent implements OnInit {
-  questions:Question[]=[]
-  constructor() { }
+  questions: Question[] = [];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
